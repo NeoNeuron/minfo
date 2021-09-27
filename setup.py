370c203d6fcfd17da0f6e5f2964bde3dc4c5e5ff
Kyle Chen <NeoNeuron@users.noreply.github.com>
2021-09-27 22:33:57 +0800

    Update build.yml

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ ext_modules = [
         "minfo.mi_float", 
         sources=["src/minfo.pyx", "src/mutual_info.cpp"], 
         language='c++',
-        extra_compile_args=['-O2', '-std=c++11', '-fopenmp'] 
+        extra_compile_args=['-O2', '-std=c++11', '-fopenmp'],
             # + ['-fopenmp=libomp5' if sys.platform.startswith('darwin') else '-fopenmp'],
         include_dirs=['include/', numpy.get_include()]
     )
